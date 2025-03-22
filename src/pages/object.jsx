ef700788f64510ae466b93e8aa67947b53756517
BekhruzMansurov04 <133450536+BekhruzMansurov04@users.noreply.github.com>
2025-03-22 06:47:36 +0500

    Update object.jsx

--- src/pages/object.jsx
+++ src/pages/object.jsx
@@ -2,14 +2,12 @@ import React, { useState, useEffect } from "react";
 
 function Object() {
   const [users, setUsers] = useState([]);
-
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((res) => res.json())
       .then((data) => setUsers(data))
       .catch((error) => console.error("Error fetching users:", error));
   }, []);
-
   return (
     <div className="max-w-6xl mx-auto p-8 bg-gray-100 min-h-screen">
       <h1 className="text-3xl font-bold text-center mb-8 text-gray-800">Posts</h1>
